d6f67f8e4a9868117251f918806b4508491e9779
Fahim Nasir <fahimnasir1993@gmail.com>
2024-01-18 20:49:41 +0500

    Email module added

--- src/routes/webuser/auth/signin.ts
+++ src/routes/webuser/auth/signin.ts
@@ -3,8 +3,14 @@ import { AppUser } from "../../../models/AppUser";
 import { ApiResponseDto } from "../../../dto/api-response.dto";
 import { Password } from "../../../services/password";
 import jwt from "jsonwebtoken";
+import { sendEmail } from "../../../services/mailer";
 
 const router = express.Router();
+
+router.post("/api/users/testmail", async (req: Request, res: Response) => {
+  sendEmail("fahimnasir1993@gmail.com", "Test Email", "Code is 1234");
+  res.send("Completed");
+});
 
 router.post("/api/users/signin", async (req: Request, res: Response) => {
   try {
